2b4c0c7b950d7dfde5ff6a020e3e06d5f54d280e
furyton <1543159426@qq.com>
2021-08-09 17:05:33 +0800

    update usage

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 # MentorNet
 pytorch version
 
-well, I'm still working on it, nothing happens in this repo right now.
+Related paper:
+**<a href="https://arxiv.org/abs/1712.05055">MentorNet: Learning Data-Driven Curriculum for Very Deep Neural Networks on Corrupted Labels
+</a>**
+<br>
+Lu Jiang, Zhengyuan Zhou, Thomas Leung, Li-Jia Li, Li Fei-Fei
+<br>
+Presented at [ICML 2018](https://icml.cc/Conferences/2018)
+
+Related code:
+[MentorNet(google)](https://github.com/google/mentornet)
+
+
+## usage (how to train mentornet (NOT with StudentNet))
+
+- first you need to train your student model on a noisy dataset which you have a corresponding clean version.
+- store the loss, epoch and label in a csv file. the format is as below:
+  ```
+  'id' 'epoch' 'noisy label' 'clean label' 'loss on the noisy label'
+  ...
+  ```
+  there is a sample csv file `fake_data.csv`
+- if you want to preprocess the csv file, use these command:
+  ```
+  python main.py --process_data=true --raw_csv="\path" --data_path="save\path\to"
+  ```
+
+  if you want to train mentornet on the dataset, use these:
+  ```
+  python main.py --process_data=false --processed_path="\path\to\blah_percentile_40" --epoch=10 --device="cpu" --batch_size=32 --show_progress_bar=false
+  ```
+
 
 UPDATE:
 - 8.2.2020: add mentornet_nn class, it works but I am not sure if it is correct.
--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+import argparse
+from ast import arg, parse
 import training_mentornet.train as train
+import training_mentornet.data_generator as data_generator
+
+csv_file_path = "/data/lizongbu-slurm/furyton/mentornet/MentorNet_pytorch/fake_data.csv"
+
+preprocess_data_path = "processed_data/fake_data_percentile_40"
+
+def str2bool(v):
+    return v.lower() in ('true')
 
 if __name__ == '__main__':
-    tr = train.trainer(train_dir="trial", data_path="/data/lizongbu-slurm/furyton/mentornet/MentorNet_pytorch/training_mentornet/processed_data/fake_data_percentile_40", show_progress_bar=False, epoch=10)
+    parser = argparse.ArgumentParser()
 
-    tr.train()
+    parser.add_argument('--process_data', type=str2bool)
+    parser.add_argument('--raw_csv', type=str, default=None, help="raw csv file path")
+    parser.add_argument('--data_path', type=str, default=None, help="where you want to save the processed dataset")
+    parser.add_argument('--processed_path', type=str, default=None)
+    parser.add_argument('--train_dir', type=str, default='trial')
+    parser.add_argument('--epoch',type=int, default=10)
+    parser.add_argument('--device',type=str,default='cpu')
+    parser.add_argument('--lr',type=float,default=0.1)
+    parser.add_argument('--batch_size', type=int,default=32)
+    parser.add_argument('--show_progress_bar',type=str2bool, default=False)
+
+    config = parser.parse_args()
+
+    if config.process_data:
+        data_generator.generate_data_driven(config.raw_csv, config.data_path)
+    else:
+        tr = train.trainer(train_dir=config.train_dir, data_path=config.processed_path, show_progress_bar=config.show_progress_bar, epoch=config.epoch, mini_batch_size=config.batch_size, device=config.device)
+
+        tr.train()
